3362dd458b77d68ca3fcf8d9bd6083b931449544
VovaVal <malinskiyvova@gmail.com>
2026-05-14 09:05:20 +0300

    Added serve

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ app = create_app()
 
 if __name__ == '__main__':
     # app.run(host='0.0.0.0', port=8080, debug=True)
-    serve(host='0.0.0.0', port=5000)
+    serve(app, host='0.0.0.0', port=5000)
