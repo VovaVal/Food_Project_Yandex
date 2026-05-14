e2e23c5b0f5be727b1b4deb0a78b69334c60122c
VovaVal <malinskiyvova@gmail.com>
2026-05-14 12:57:50 +0300

    Changed the db_session.py

--- website/routes/shops.py
+++ website/routes/shops.py
@@ -326,7 +326,7 @@ def edit_settings_shop(shop_id: int):
             'address': address,
             'coords': coords,
             'description': description,
-            'delivery_radius': int(delivery_radius) if delivery_radius.is_integer() else 0,
+            'delivery_radius': int(delivery_radius) if (isinstance(delivery_radius, float)) else 0,
             'timetable': schedule
         }
 
